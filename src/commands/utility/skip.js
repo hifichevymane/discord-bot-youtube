58b253c4a685bfd3a73b5ec6082c009a12e1c4ba
hifichevymane <blekbjek@gmail.com>
2024-11-11 22:12:10 +0200

    fix the bug with double track skipping

--- src/commands/utility/skip.js
+++ src/commands/utility/skip.js
@@ -1,8 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { AudioPlayerStatus } from '@discordjs/voice';
-import { getNextTrack, getTrackQueue } from '../../track-queue.js';
 import player from '../../audio-player.js';
-import { createAudioResourceFromYouTubeURL } from '../../utils.js';
 
 const data = new SlashCommandBuilder()
   .setName('skip')
@@ -15,13 +13,7 @@ const execute = async (interaction) => {
   }
 
   player.stop();
-  const nextTrackUrl = getNextTrack();
   await interaction.reply('The current track was skipped!');
-
-  if (nextTrackUrl) {
-    const resource = createAudioResourceFromYouTubeURL(nextTrackUrl);
-    player.play(resource);
-  }
 };
 
 export { data, execute };
