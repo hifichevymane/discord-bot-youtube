1fbc9cfa31f495b17b7b19a81f1011892e088b29
hifichevymane <blekbjek@gmail.com>
2024-11-12 20:37:59 +0200

    don't need this import

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { Client, Events, GatewayIntentBits, Collection } from 'discord.js';
+import { Client, GatewayIntentBits, Collection } from 'discord.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
