f283c04a26fd7c3d79dceaf7214aaa8882427207
hifichevymane <blekbjek@gmail.com>
2024-11-22 19:16:08 +0200

    a little custom audio player refactoring

--- src/audio-player.ts
+++ src/audio-player.ts
@@ -6,7 +6,24 @@ import { createAudioResourceFromYouTubeURL } from './utils';
 import { client } from './client';
 
 class CustomAudioPlayer extends AudioPlayer {
-  currentTextChannelId = '';
+  private currentTextChannelId: string | undefined;
+  private currentVideoURL: string | undefined;
+
+  public getCurrentTextChannelId(): string | undefined {
+    return this.currentTextChannelId;
+  }
+
+  public setCurrentTextChannelId(channelId: string): void {
+    this.currentTextChannelId = channelId;
+  }
+
+  public getCurrentVideoURL(): string | undefined {
+    return this.currentVideoURL;
+  }
+
+  public setCurrentVideoURL(url: string): void {
+    this.currentVideoURL = url;
+  }
 };
 
 const player = new CustomAudioPlayer();
@@ -17,9 +34,12 @@ player.on(AudioPlayerStatus.Idle, async () => {
   const resource = createAudioResourceFromYouTubeURL(url);
   player.play(resource);
 
-  const currentTextChannelId = player.currentTextChannelId;
+  const currentTextChannelId = player.getCurrentTextChannelId();
+  if (!currentTextChannelId) return;
+
   const channel = client.channels.cache.get(currentTextChannelId) as TextChannel | undefined;
   if (!channel) return;
+
   await channel.send(`Now is playing: ${url}`);
 });
 
